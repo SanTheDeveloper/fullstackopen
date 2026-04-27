b2e03c0a514ce49cf25362c92f8d9e3dc1e7eeb0
Sandeep Rout <sandeeprout.developer@gmail.com>
2026-04-27 14:29:26 +0530

    refactor: consolidate all course data into a single object (exercise 1.5)

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
 };
 
 const Part = ({ part }) => {
@@ -10,12 +10,12 @@ const Part = ({ part }) => {
   );
 };
 
-const Content = ({ parts }) => {
+const Content = ({ course }) => {
   return (
     <>
-      <Part part={parts[0]} />
-      <Part part={parts[1]} />
-      <Part part={parts[2]} />
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
     </>
   );
 };
@@ -30,27 +30,29 @@ const Total = ({ parts }) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header name={course.name} />
+      <Content course={course} />
+      <Total parts={course.parts} />
     </div>
   );
 };
