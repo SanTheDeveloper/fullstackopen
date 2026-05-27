a5d5b809aace032d579d4bc5b745ab96ad63d230
Sandeep Rout <123567730+SanTheDeveloper@users.noreply.github.com>
2026-05-27 19:34:32 +0530

    feat: implement controlled form to add names to phonebook (exercise 2.6)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,21 +1,48 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
   const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    if (!newName) return;
+
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+    };
+    setPersons([...persons, personObject]);
+    setNewName("");
+  };
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name:{" "}
+          <input
+            value={newName}
+            onChange={handlePersonChange}
+            placeholder="Add Name"
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
