0107231a1fa92316135dac86837a37ea554c6eb1
Sandeep Rout <123567730+SanTheDeveloper@users.noreply.github.com>
2026-05-27 20:54:50 +0530

    feat: implement case-insensitive search filter using derived state (exercise 2.9)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", id: 1, number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [personFilter, setPersonFilter] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -43,10 +47,31 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleFilterChange = (event) => {
+    setPersonFilter(event.target.value);
+  };
+
+  const personToShow = personFilter
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(personFilter.toLowerCase()),
+      )
+    : persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with{" "}
+        <input
+          value={personFilter}
+          onChange={handleFilterChange}
+          placeholder="Filter Name"
+        />
+      </div>
+
       <form onSubmit={addPerson}>
+        <h2>add a new</h2>
         <div>
           name:{" "}
           <input
@@ -69,7 +94,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {personToShow.map((person) => (
           <li key={person.id}>
             {person.name} {person.number}
           </li>
