824d67c6b9c1202ebd2c2615ee2689af3254e9a7
Sandeep Rout <123567730+SanTheDeveloper@users.noreply.github.com>
2026-06-09 19:53:02 +0530

    feat: implement REST API with Express, CRUD operations, and Morgan logging (exercises 3.1-3.8)

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.use(express.json());
+
+morgan.token("body", (request, response) => {
+  if (request.method === "POST") {
+    return JSON.stringify(request.body);
+  }
+  return "";
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello Backend</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const date = new Date();
+
+  const html = `
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${date}</p>
+  `;
+  response.send(html);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+const generateRandomId = () => {
+  return String(Math.floor(Math.random() * 1000000));
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number is missing",
+    });
+  }
+
+  const findDuplicateName = persons.some(
+    (person) => person.name.toLowerCase() === body.name.toLowerCase(),
+  );
+
+  if (findDuplicateName) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateRandomId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.status(201).json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
