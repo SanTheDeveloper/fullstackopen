86bd659f09a839e11fd27818ffcbcdb8af17f721
Sandeep Rout <123567730+SanTheDeveloper@users.noreply.github.com>
2026-06-11 18:39:18 +0530

    build(backend): install cors middleware and configure express to serve static frontend dist folder

--- notes-app/backend/index.js
+++ notes-app/backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 let notes = [
@@ -27,8 +28,10 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(express.json());
 app.use(requestLogger);
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -89,7 +92,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
