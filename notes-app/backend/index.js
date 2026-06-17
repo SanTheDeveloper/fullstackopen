500e18533a08221a0c674b9023142f115770f439
Sandeep Rout <123567730+SanTheDeveloper@users.noreply.github.com>
2026-06-17 18:52:38 +0530

    feat(backend): install mongoose and create standalone mongo.js script to test MongoDB Atlas connection

--- notes-app/backend/index.js
+++ notes-app/backend/index.js
@@ -27,9 +27,9 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(requestLogger);
-app.use(express.static("dist"));
 app.use(express.json());
+app.use(express.static("dist"));
+app.use(requestLogger);
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
