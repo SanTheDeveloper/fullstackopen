00e9ed9e31336f4be73bc1ec7378e65f854a2632
Sandeep Rout <123567730+SanTheDeveloper@users.noreply.github.com>
2026-06-19 13:50:31 +0530

    feat(phonebook-backend): integrate MongoDB to fetch and save contacts and secure credentials via dotenv

--- phonebook-app/backend/index.js
+++ phonebook-app/backend/index.js
@@ -1,30 +1,11 @@
+require("dotenv").config();
 const express = require("express");
+const Person = require("./models/person");
 const morgan = require("morgan");
 
 const app = express();
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+let persons = [];
 
 app.use(express.json());
 app.use(express.static("dist"));
@@ -45,7 +26,9 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
@@ -70,10 +53,6 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-const generateRandomId = () => {
-  return String(Math.floor(Math.random() * 1000000));
-};
-
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
@@ -83,25 +62,24 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  const findDuplicateName = persons.some(
-    (person) => person.name.toLowerCase() === body.name.toLowerCase(),
-  );
+  // const findDuplicateName = persons.some(
+  //   (person) => person.name.toLowerCase() === body.name.toLowerCase(),
+  // );
 
-  if (findDuplicateName) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
+  // if (findDuplicateName) {
+  //   return response.status(400).json({
+  //     error: "name must be unique",
+  //   });
+  // }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateRandomId(),
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.status(201).json(person);
+  person.save().then((savedPerson) => {
+    response.status(201).json(savedPerson);
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -112,7 +90,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
