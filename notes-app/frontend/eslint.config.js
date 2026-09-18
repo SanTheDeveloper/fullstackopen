1efd807adac4207e8708c46b58518c5472e4ff89
Sandeep Rout <123567730+SanTheDeveloper@users.noreply.github.com>
2026-09-18 12:47:42 +0530

    refactor(notes-app): practice props children and component refs

--- notes-app/frontend/eslint.config.js
+++ notes-app/frontend/eslint.config.js
@@ -1,21 +1,53 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
+
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
+
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+
     languageOptions: {
+      ecmaVersion: "latest",
       globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      "react-hooks/set-state-in-effect": "off",
+
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "no-console": "off",
     },
   },
-])
+]);
